638524e23d98e3b0dc9c03544080fd277fcf3192
Oleh Prypin <oleh@pryp.in>
2020-09-20 17:23:29 +0200

    Change release method

--- release.sh
+++ release.sh
@@ -1,70 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# fail on unset variables and command errors
-set -eu -o pipefail # -x: is for debugging
+set -e -u -x
 
-DEFAULT_BRANCH="main"
+cd "$(dirname "$0")"
 
-CURRENT_BRANCH="$(git branch --show-current)"
-if [ "${CURRENT_BRANCH}" != "${DEFAULT_BRANCH}" ]; then
-  echo "$0: Current branch ${CURRENT_BRANCH} is not ${DEFAULT_BRANCH}, continue? (y/n)"
-  read -r res
-  if [ "${res}" = "n" ]; then
-    echo "$0: Stop script"
-    exit 0
-  fi
-fi
-
-PRERELEASE_TYPE_LIST="prerelease prepatch preminor premajor"
-if [ "${CURRENT_BRANCH}" != "${DEFAULT_BRANCH}" ]; then
-  RELEASE_TYPE_LIST="${PRERELEASE_TYPE_LIST}"
-else
-  RELEASE_TYPE_LIST="${PRERELEASE_TYPE_LIST} patch minor major"
-fi
-
-if command -v fzf; then
-  RELEASE_TYPE=$(echo "${RELEASE_TYPE_LIST}" | tr ' ' '\n' | fzf --layout=reverse)
-else
-  select sel in ${RELEASE_TYPE_LIST}; do
-    RELEASE_TYPE="${sel}"
-    break
-  done
-fi
-
-echo "$0: Create ${RELEASE_TYPE} release, continue? (y/n)"
-read -r res
-if [ "${res}" = "n" ]; then
-  echo "$0: Stop script"
-  exit 0
-fi
-
-git fetch origin
-if [ "${CURRENT_BRANCH}" != "${DEFAULT_BRANCH}" ]; then
-  git pull origin "${CURRENT_BRANCH}"
-else
-  git pull origin ${DEFAULT_BRANCH}
-  git tag -d v3 || true
-  git pull origin --tags
-fi
-
-npm ci
-
-mkdir ./lib
-npm run build
-git add ./lib/index.js
-git commit -m "chore(release): Add build assets"
-
-npm run release -- --release-as "${RELEASE_TYPE}" --preset eslint
-
-git rm ./lib/index.js
-rm -rf ./lib
-git commit -m "chore(release): Remove build assets [skip ci]"
-
-if [ "${CURRENT_BRANCH}" != "${DEFAULT_BRANCH}" ]; then
-  git push origin "${CURRENT_BRANCH}"
-else
-  git push origin ${DEFAULT_BRANCH}
-fi
-
-TAG_NAME="v$(jq -r '.version' ./package.json)"
-git push origin "${TAG_NAME}"
+rm -rf node_modules
+git checkout v3
+GIT_EDITOR=true git merge main
+sed -i -E -e 's/"version": ".+",/"version": "'"$1"'",/' package.json
+git diff --quiet && false
+rm -rf node_modules
+npm install --production
+git add -f node_modules package.json package-lock.json
+git diff --quiet
+git commit -m "v$1"
+git tag "v$1"
+echo git push origin main v3 --tags
